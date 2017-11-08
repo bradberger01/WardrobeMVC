2306d1d0e556708f473e0e2c07385497e2553d0d
bradberger01 <bradberger@hotmail.com>
2017-11-08 13:56:30 -0500

    change to database path

--- WardrobeMVC/Controllers/ShoesController.cs
+++ WardrobeMVC/Controllers/ShoesController.cs
@@ -12,7 +12,7 @@ namespace WardrobeMVC.Controllers
 {
     public class ShoesController : Controller
     {
-        private WardrobeMVCContext db = new WardrobeMVCContext();
+        private WardrobeMVCEntities db = new WardrobeMVCEntities();
 
         // GET: Shoes
         public ActionResult Index()
